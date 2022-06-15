1d096b63608491fdaeb6b856a22debffff8e8ff1
Kevin Tse <ktse@fb.com>
2022-06-15 19:42:04 -0400

    [DataPipe] Simple graph fast-forwarding
    ghstack-source-id: 1484c43dd0c0aa8daa069c60899bf909869e7bd0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/79479

--- torch/utils/data/datapipes/_hook_iterator.py
+++ torch/utils/data/datapipes/_hook_iterator.py
@@ -126,6 +126,16 @@ def hook_iterator(namespace, profile_name):
         def wrap_generator(*args, **kwargs):
             gen = func(*args, **kwargs)
             datapipe = args[0]
+            if datapipe._fast_forward_iterator:
+                print("Returning fast-forward iterator")
+                print("Iterator in wrap_generator", datapipe._fast_forward_iterator)
+                # print(list(datapipe._fast_forward_iterator))
+                while True:
+                    try:
+                        yield next(datapipe._fast_forward_iterator)
+                    except StopIteration:
+                        datapipe._fast_forward_iterator = None
+                        return
             iterator_id = _set_datapipe_valid_iterator_id(datapipe)  # This ID is tied to each created iterator
             _profiler_enabled = torch.autograd._profiler_enabled()
             try:
@@ -185,6 +195,10 @@ def hook_iterator(namespace, profile_name):
         def wrap_iter(*args, **kwargs):
             iter_ret = func(*args, **kwargs)
             datapipe = args[0]
+            # TODO: Check if I need to add fast-forward iterator related logic here...
+            #       Probably something related to skipping `_set_valid_iterator_id` and `reset`
+            #       It depends on what the return is
+            #       Maybe `if datapipe._fast_forward_iterator:` return `iter_ret = datapipe._fast_forward_iterator`
             iterator_id = _set_datapipe_valid_iterator_id(datapipe)  # This ID is tied to each created iterator
             return IteratorDecorator(iter_ret, datapipe, iterator_id)
 
